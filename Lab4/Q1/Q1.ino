5a73699bf2f7bcbbc8e9e9456fa738a80384ed6e
Radin Shayanfar <radin.shayanfar@gmail.com>
2020-10-27 02:24:24 +0330

    Lab4 Q1 added.

---
+++ Lab4/Q1/Q1.ino
@@ -0,0 +1,21 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(2);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  for (pos = 0; pos <= 180; pos += 1) {
+    myservo.write(pos);
+    delay(15);
+  }
+  for (pos = 180; pos >= 0; pos -= 1) {
+    myservo.write(pos);
+    delay(15);
+  }
+}
