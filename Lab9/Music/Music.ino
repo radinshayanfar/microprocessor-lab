1cc8a50a892e860729212211fc3defa35b070ffd
Radin Shayanfar <radin.shayanfar@gmail.com>
2020-12-29 02:08:53 +0330

    Lab9 pause/play button added.

--- Lab9/Music/Music.ino
+++ Lab9/Music/Music.ino
@@ -1,30 +1,38 @@
 #include "pitches.h"
 
-//#include "jingle_bells.h"
+#include "jingle_bells.h"
 //#include "Ode_to_Joy.h"
 //#include "the_imperial_march.h"
 //#include "frere_jacques.h"
-#include "ey_iran.h"
+//#include "ey_iran.h"
 
-//#define TEMPO 180
+#define TEMPO 210
 //#define TEMPO 150
 //#define TEMPO 150
 //#define TEMPO 135
-#define TEMPO 210
+//#define TEMPO 210
 
-const int potPin = A0, soundPin = 8;
+const int potPin = A0, buttonPin = 0, soundPin = 8;
 
+bool play = false;
 float scale;
 
 void setup() {
-
+  pinMode(buttonPin, INPUT_PULLUP);
 }
 
 void loop() {
-  for (int note = 0; note < sizeof(melody) / sizeof(int); note++) {
-    scale = analogRead(potPin) / 512.0;
-    int duration = 2 * 60 * 1000.0 / (TEMPO * noteDurations[note]);
-    tone(soundPin, (int) (melody[note] * scale), duration);
-    delay( (int) duration );
+  play = 1 - digitalRead(buttonPin);
+  
+  if (play) {
+    for (int note = 0; note < sizeof(melody) / sizeof(int); note++) {
+      scale = analogRead(potPin) / 512.0;
+      int duration = 2 * 60 * 1000.0 / (TEMPO * noteDurations[note]);
+      tone(soundPin, (int) (melody[note] * scale), duration);
+      delay( (int) duration );
+      if (digitalRead(buttonPin)) {
+        break;
+      }
+    }
   }
 }
